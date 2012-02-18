45dc11f2bcf606ec0ef0843e9d92385bb15ec49f
Belén Albeza <belen.albeza@gmail.com>
2012-02-18 19:01:29 +0000

    hack to bypass error with pearson

--- models/pearson.rb
+++ models/pearson.rb
@@ -11,7 +11,9 @@ class Pearson
     buffer = open(url, "UserAgent" => "Ruby-Wget").read
     result = JSON.parse(buffer)
 
-    @word = result['Entry']['Head']['HWD']['#text']
-    @desc = result['Entry']['Sense']['DEF']['#text']
+    # @word = result['Entry']['Head']['HWD']['#text']
+    # @desc = result['Entry']['Sense']['DEF']['#text']
+    @word = 'analytical'
+    @desc = 'thinking about things in a detailed and intelligent way, so that you can examine and understand things'
   end
 end
