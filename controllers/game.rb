362740a13d57f6b7e61eeb6a07200e89082baf1d
Belén Albeza <belen.albeza@gmail.com>
2012-02-18 19:07:13 +0000

    guessing letter now takes argument from headers

--- controllers/game.rb
+++ controllers/game.rb
@@ -8,9 +8,10 @@ post '/game/:game_id/clue' do
   return JSON.dump(status)
 end
 
-post '/game/:game_id/guess/:letter' do
+post '/game/:game_id/guess/?' do
   channel = GameChannel.new('111')
-  right_count = @game.guess_by_player(@user, '111', @letter)
+  letter = params[:letter]
+  right_count = @game.guess_by_player(@user, '111', letter)
   if right_count > 0
     channel.publish_guess_right(@user, right_count)
   else
@@ -21,7 +22,7 @@ post '/game/:game_id/guess/:letter' do
   return JSON.dump(status)
 end
 
-post '/game/:game_id/solve/' do
+post '/game/:game_id/solve/?' do
   channel = GameChannel.new('111')
   solution = params['solution']
   
